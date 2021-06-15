9023db3701e399552523f752d0c306b66c0e9690
Ted Keim <tedkeim@yahoo.com>
2021-06-15 15:28:27 -0400

    added const express, routes and port to server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require("./routes/apiRoutes")
+
+const app = express();
+const PORT = process.env.PORT || 8080;
