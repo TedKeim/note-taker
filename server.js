92256f30528af277de52518808d50993315be35f
Ted Keim <tedkeim@yahoo.com>
2021-09-06 08:15:05 -0400

    added html routes to server.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 
-require('./routes/routes')(app);
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
