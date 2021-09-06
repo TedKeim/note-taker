c9b699dd4ff089c6e2d99cdb7b1af33c6f5dfdcc
Ted Keim <tedkeim@yahoo.com>
2021-09-06 09:09:47 -0400

    updated delete note route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 const { v4: uuidv4 } = require('uuid');
+const Middle = require('../db/middleware')
 
 
 module.exports = (app) => {
@@ -10,6 +11,13 @@ module.exports = (app) => {
   app.get("/api/notes/:id", (req, res) => {
     res.json(data[Number(req.params.id)]);
   });
+
+  router.delete('/api/notes/:id', (req, res) => {
+    const deleteId = req.params.id
+
+    Middle.deleteNote(deleteId)
+    res.json("true")
+  })
 
   app.post("/api/notes", (req, res) => {
     let note = req.body;
