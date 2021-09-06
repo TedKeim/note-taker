620221eef9a010a68983120f5c5a1aef0a851e8b
Ted Keim <tedkeim@yahoo.com>
2021-09-06 07:56:05 -0400

    added app.get routes

--- routes/routes.js
+++ routes/routes.js
@@ -8,3 +8,23 @@ module.exports = app => {
         if (err) throw err;
 
         var notes = JSON.parse(data);
+
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
+
+
+
+        app.get("/api/notes/:id", function(req,res) {
+            res.json(notes[req.params.id]);
+        });
+
+
+        app.get('/notes', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+
+
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
