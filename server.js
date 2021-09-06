6177a0c816172476b42852a8b4ee50fb8f56596d
Ted Keim <tedkeim@yahoo.com>
2021-09-06 07:54:25 -0400

    started routes.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express")
-const htmlRoutes = require('./routes/htmlRoutes')
-const apiRoutes = require("./routes/apiRoutes")
+const fs = require("fs")
+const path = require("path")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
+
+require('./routes/routes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
